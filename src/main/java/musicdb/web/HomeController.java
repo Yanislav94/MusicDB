cd06f40e019cb0147794eed3040b076267d7b8da
Yanislav <ytomo@softserveinc.com>
2022-06-23 18:33:31 +0300

    More development

--- src/main/java/musicdb/web/HomeController.java
+++ src/main/java/musicdb/web/HomeController.java
@@ -1,10 +1,14 @@
 package musicdb.web;
 
+import musicdb.model.entity.User;
 import musicdb.service.AlbumService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpSession;
+import java.util.Optional;
 
 
 @Controller
@@ -19,12 +23,12 @@ public class HomeController {
     @GetMapping("/")
     public ModelAndView index(HttpSession httpSession, ModelAndView modelAndView) {
         modelAndView.setViewName("home");
-        modelAndView.addObject("wtfbrat", "dam");
-        if (httpSession.getAttribute("user") == null) {
-            modelAndView.setViewName("index");
-        } else {
-            modelAndView.addObject("albums", this.albumService.findAllItems());
-        }
+        modelAndView.addObject("albums", "vankoethebest");
+//        if (httpSession.getAttribute("user") == null) {
+//            modelAndView.setViewName("index");
+//        } else {
+//
+//        }
 
         return modelAndView;
     }
